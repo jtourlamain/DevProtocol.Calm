16c019ee0ddc6f276f6e33cf908a2397b73d4808
Jan Tourlamain <jan.tourlamain@outlook.com>
2019-04-16 21:10:53 +0200

    setup Calm core

---
+++ src/Json.fs
@@ -0,0 +1,13 @@
+module DevProtocol.Calm.Json
+
+open Newtonsoft.Json
+
+let serialize obj = 
+    JsonConvert.SerializeObject obj
+
+let deserialize<'a> str = 
+    try
+        JsonConvert.DeserializeObject<'a> str
+        |> Result.Ok
+    with
+        | ex -> Result.Error ex
